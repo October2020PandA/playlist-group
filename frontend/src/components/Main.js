4c5bba4b94e96b264be1f633acb3ebdba3d65ce8
williamgilbreath <williamgilbreath28@gmail.com>
2020-10-22 12:57:23 -0500

    UI Work

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -6,18 +6,11 @@ const Main = () => {
     return(
         <div className={`container-fluid`}>
             <div class="row">
-                <div class="col-8"></div>
-                <div className="col-4">
-                    <p>Let's get started...</p>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-8"></div>
-                <Paper elevation={3} className={`${styles.paper} col-4`}>
+                <div class="col-7" />
+                <Paper elevation={7} className={`${styles.paper} col-4`}>
+                    <p>Go ahead and enter in a song...</p>
                     <input type="text" className="mb-3" placeholder="(Enter a song)"></input>
                     <p>How do you want to create your playlist?</p>
-
                     <select name="playlistType" className={`mt-0`}>
                         <option value="BPM">BPM</option>
                         <option value="Tempo">Tempo</option>
