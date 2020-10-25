7b718166370686bd5ea7a7e84e054fdd2a83f691
Danny Pham <dpham088@gmail.com>
2020-10-24 17:35:24 -0700

    cleaned up code

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -4,21 +4,14 @@ import styles from '../modules/Main.module.css';
 
 const Main = () => {
     return(
-        <div className={`container-fluid`}>
+        <div>
             <div class="row">
-                <div class="col-8"></div>
-                <div className="col-4">
-                    <p>Let's get started...</p>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-8"></div>
-                <Paper elevation={3} className={`${styles.paper} col-4`}>
-                    <input type="text" className="mb-3" placeholder="(Enter a song)"></input>
-                    <p>How do you want to create your playlist?</p>
-
-                    <select name="playlistType" className={`mt-0`}>
+                <div class="col-7" />
+                <Paper elevation={7} className={`${styles.paper} col-4`}>
+                    <p className={`${styles.header}`}>Go ahead and enter in a song...</p>
+                    <input type="text" className="mb-3" placeholder="Song..." className={`${styles.songField}`}></input>
+                    <p className={`${styles.header}`}>How should we make your playlist?</p>
+                    <select name="playlistType" className={`mt-0 ${styles.selectField}`}>
                         <option value="BPM">BPM</option>
                         <option value="Tempo">Tempo</option>
                         <option value="Energy">Energy</option>
